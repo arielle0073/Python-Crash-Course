5d6c9ea36b616b0c7e52ba92fc9cc16c28629b22
arielle0073 <62905999+arielle0073@users.noreply.github.com>
2020-06-06 19:31:29 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-alien_color == 'yellow'
-print(f"\n{alien_color}")
-
-if alien_color == 'green':
-    print("You just earned 5 points!")
-elif alien_color == 'yellow':
-    print("You just earned 10 points!")
-else:
-    print("You just earned 15 points!")
