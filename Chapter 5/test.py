b50f5a8be23ba69cad78a85637793b33f8804120
arielle0073 <62905999+arielle0073@users.noreply.github.com>
2020-06-06 18:37:42 -0400

    Uploaded Files
    Chapter Exercises

---
+++ Chapter 5/test.py
@@ -0,0 +1,9 @@
+alien_color == 'yellow'
+print(f"\n{alien_color}")
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
