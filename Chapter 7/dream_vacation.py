4a1b4d8037f854620d0e4c583854c16dc40e2c70
Arielle Waller <62905999+ariellewaller@users.noreply.github.com>
2020-06-26 22:16:35 -0400

    Add files via upload

---
+++ Chapter 7/dream_vacation.py
@@ -0,0 +1,58 @@
+#	7-10. Dream Vacation: Write a program that polls users about their dream
+#   vacation. Write a prompt similar to If you could visit one place in the
+#   world, where would you go? Include a block of code that prints the results
+#   of the poll.
+
+# message = ""
+
+# poll_results = {} 
+
+# SOLUTION 1 
+# while message != 'no': 
+#     name_prompt = "What is your name? " 
+#     vacation_prompt = "If you could visit one place in the world, where would you go? "
+#     name = input(name_prompt)
+#     vacation_spot = input(vacation_prompt)
+#     poll_results[name] = vacation_spot
+#     message = input("Would you like to let another person respond? (yes/no) ")
+
+# for name, vacation_spot in poll_results.items():
+#     print(f"{name.title()} would like to visit {vacation_spot.title()}")
+
+# SOLUTION 2
+
+# poll_active = True 
+
+# poll_results = {}
+
+# while poll_active: 
+#     name_prompt = "What is your name? " 
+#     vacation_prompt = "If you could visit one place in the world, where would you go? "
+#     name = input(name_prompt)
+#     vacation_spot = input(vacation_prompt)
+#     poll_results[name] = vacation_spot
+#     message = input("Would you like to let another person respond? (yes/no) ")
+#     if message == 'no':
+#         poll_active = False 
+
+# for name, vacation_spot in poll_results.items():
+#     print(f"{name.title()} would like to visit {vacation_spot.title()}")
+
+
+#   SOLUTION 3
+
+
+poll_results = {}
+
+while True: 
+    name_prompt = "What is your name? " 
+    vacation_prompt = "If you could visit one place in the world, where would you go? "
+    name = input(name_prompt)
+    vacation_spot = input(vacation_prompt)
+    poll_results[name] = vacation_spot
+    message = input("Would you like to let another person respond? (yes/no) ")
+    if message == 'no':
+        break
+
+for name, vacation_spot in poll_results.items():
+    print(f"{name.title()} would like to visit {vacation_spot.title()}")
