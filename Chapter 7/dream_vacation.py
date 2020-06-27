2210560551c5f477ceeb66501cce132305b6dc03
Arielle Waller <62905999+ariellewaller@users.noreply.github.com>
2020-06-26 22:23:37 -0400

    Add files via upload

--- Chapter 7/dream_vacation.py
+++ Chapter 7/dream_vacation.py
@@ -1,16 +1,17 @@
 #	7-10. Dream Vacation: Write a program that polls users about their dream
-#   vacation. Write a prompt similar to If you could visit one place in the
-#   world, where would you go? Include a block of code that prints the results
-#   of the poll.
-
-# message = ""
-
-# poll_results = {} 
+#	vacation. Write a prompt similar to If you could visit one place in the
+#	world, where would you go? Include a block of code that prints the results
+#	of the poll.
 
 # SOLUTION 1 
+
+# message = ""
+# poll_results = {} 
+
+# name_prompt = "What is your name? " 
+# vacation_prompt = "If you could visit one place in the world, where would you go? "
+
 # while message != 'no': 
-#     name_prompt = "What is your name? " 
-#     vacation_prompt = "If you could visit one place in the world, where would you go? "
 #     name = input(name_prompt)
 #     vacation_spot = input(vacation_prompt)
 #     poll_results[name] = vacation_spot
@@ -22,12 +23,12 @@
 # SOLUTION 2
 
 # poll_active = True 
-
 # poll_results = {}
 
+# name_prompt = "What is your name? " 
+# vacation_prompt = "If you could visit one place in the world, where would you go? "
+
 # while poll_active: 
-#     name_prompt = "What is your name? " 
-#     vacation_prompt = "If you could visit one place in the world, where would you go? "
 #     name = input(name_prompt)
 #     vacation_spot = input(vacation_prompt)
 #     poll_results[name] = vacation_spot
@@ -41,12 +42,12 @@
 
 #   SOLUTION 3
 
+name_prompt = "What is your name? " 
+vacation_prompt = "If you could visit one place in the world, where would you go? "
 
 poll_results = {}
 
 while True: 
-    name_prompt = "What is your name? " 
-    vacation_prompt = "If you could visit one place in the world, where would you go? "
     name = input(name_prompt)
     vacation_spot = input(vacation_prompt)
     poll_results[name] = vacation_spot
