1057401d2a4894cee238ef09df638e65cedf9b4f
Arielle Waller <62905999+ariellewaller@users.noreply.github.com>
2020-06-26 22:56:46 -0400

    Add files via upload

---
+++ message.py
@@ -0,0 +1,13 @@
+#	8-1. Message: Write a function called display_message() that prints one
+#   sentence telling everyone what you are learning about in this chapter. Call
+#   the function, and make sure the message displays correctly.
+
+def display_message(): 
+    """Display one sentence about what I'm learning in this chapter"""
+    print("I am learning about functions, function calls, parameters and "
+            "arguments.")
+
+
+display_message()
+
+
